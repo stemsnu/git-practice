feb1fdf7f180959d4bcd95083faa3b100a6f6652
stemsnu <stemsnu@gmail.com>
2016-09-30 20:36:12 +0900

    너무 어려워요

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "정태희싫어요"
 
 @app.route("/index")
 def index():
@@ -14,4 +14,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(port=***YOUR_PORT***)
+    app.run(host="0.0.0.0",port=55555)
